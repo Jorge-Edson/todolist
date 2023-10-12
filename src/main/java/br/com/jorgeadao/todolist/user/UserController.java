168756ff040ca9e354414be9f4241594000fde24
Jorge-Edson <Jorge-Edson@users.noreply.github.com>
2023-10-12 19:34:52 -0300

    First Commit

---
+++ src/main/java/br/com/jorgeadao/todolist/user/UserController.java
@@ -0,0 +1,21 @@
+package br.com.jorgeadao.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
+    }
+}
