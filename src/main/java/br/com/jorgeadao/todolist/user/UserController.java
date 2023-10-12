5ab5261da59af8f283bb48868767d81b0b3e2045
Jorge-Edson <Jorge-Edson@users.noreply.github.com>
2023-10-12 20:21:19 -0300

    Security update

--- src/main/java/br/com/jorgeadao/todolist/user/UserController.java
+++ src/main/java/br/com/jorgeadao/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,8 +22,12 @@ public class UserController {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
